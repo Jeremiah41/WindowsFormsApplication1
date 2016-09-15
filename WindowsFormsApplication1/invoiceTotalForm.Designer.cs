bdc530f1922193f942539ac06fb61fb1dfa5237e
Jeremiah41 <jeremiahhughes12@hotmail.ca>
2016-09-15 19:19:23 -0400

    Add project files.

---
+++ WindowsFormsApplication1/invoiceTotalForm.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class invoiceTotalForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.subTotalLabel = new System.Windows.Forms.Label();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.discountPercentLabel = new System.Windows.Forms.Label();
+            this.discountAmountLabel = new System.Windows.Forms.Label();
+            this.subTotalTextBox = new System.Windows.Forms.TextBox();
+            this.discountPercentTextBox = new System.Windows.Forms.TextBox();
+            this.discountAmountTextBox = new System.Windows.Forms.TextBox();
+            this.totalTextBox = new System.Windows.Forms.TextBox();
+            this.CalculateButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // subTotalLabel
+            // 
+            this.subTotalLabel.AutoSize = true;
+            this.subTotalLabel.Location = new System.Drawing.Point(12, 13);
+            this.subTotalLabel.Name = "subTotalLabel";
+            this.subTotalLabel.Size = new System.Drawing.Size(53, 13);
+            this.subTotalLabel.TabIndex = 0;
+            this.subTotalLabel.Text = "Sub Total";
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.AutoSize = true;
+            this.totalLabel.Location = new System.Drawing.Point(12, 116);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(31, 13);
+            this.totalLabel.TabIndex = 1;
+            this.totalLabel.Text = "Total";
+            this.totalLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // discountPercentLabel
+            // 
+            this.discountPercentLabel.AutoSize = true;
+            this.discountPercentLabel.Location = new System.Drawing.Point(12, 48);
+            this.discountPercentLabel.Name = "discountPercentLabel";
+            this.discountPercentLabel.Size = new System.Drawing.Size(89, 13);
+            this.discountPercentLabel.TabIndex = 2;
+            this.discountPercentLabel.Text = "Discount Percent";
+            // 
+            // discountAmountLabel
+            // 
+            this.discountAmountLabel.AutoSize = true;
+            this.discountAmountLabel.Location = new System.Drawing.Point(12, 85);
+            this.discountAmountLabel.Name = "discountAmountLabel";
+            this.discountAmountLabel.Size = new System.Drawing.Size(88, 13);
+            this.discountAmountLabel.TabIndex = 3;
+            this.discountAmountLabel.Text = "Discount Amount";
+            // 
+            // subTotalTextBox
+            // 
+            this.subTotalTextBox.ForeColor = System.Drawing.Color.White;
+            this.subTotalTextBox.Location = new System.Drawing.Point(127, 13);
+            this.subTotalTextBox.Name = "subTotalTextBox";
+            this.subTotalTextBox.Size = new System.Drawing.Size(100, 20);
+            this.subTotalTextBox.TabIndex = 4;
+            // 
+            // discountPercentTextBox
+            // 
+            this.discountPercentTextBox.BackColor = System.Drawing.Color.White;
+            this.discountPercentTextBox.ForeColor = System.Drawing.Color.White;
+            this.discountPercentTextBox.Location = new System.Drawing.Point(127, 45);
+            this.discountPercentTextBox.Name = "discountPercentTextBox";
+            this.discountPercentTextBox.ReadOnly = true;
+            this.discountPercentTextBox.Size = new System.Drawing.Size(100, 20);
+            this.discountPercentTextBox.TabIndex = 5;
+            // 
+            // discountAmountTextBox
+            // 
+            this.discountAmountTextBox.BackColor = System.Drawing.Color.White;
+            this.discountAmountTextBox.ForeColor = System.Drawing.Color.White;
+            this.discountAmountTextBox.Location = new System.Drawing.Point(126, 78);
+            this.discountAmountTextBox.Name = "discountAmountTextBox";
+            this.discountAmountTextBox.ReadOnly = true;
+            this.discountAmountTextBox.Size = new System.Drawing.Size(100, 20);
+            this.discountAmountTextBox.TabIndex = 6;
+            // 
+            // totalTextBox
+            // 
+            this.totalTextBox.BackColor = System.Drawing.Color.White;
+            this.totalTextBox.ForeColor = System.Drawing.Color.White;
+            this.totalTextBox.Location = new System.Drawing.Point(126, 109);
+            this.totalTextBox.Name = "totalTextBox";
+            this.totalTextBox.ReadOnly = true;
+            this.totalTextBox.Size = new System.Drawing.Size(100, 20);
+            this.totalTextBox.TabIndex = 7;
+            // 
+            // CalculateButton
+            // 
+            this.CalculateButton.Location = new System.Drawing.Point(50, 154);
+            this.CalculateButton.Name = "CalculateButton";
+            this.CalculateButton.Size = new System.Drawing.Size(75, 23);
+            this.CalculateButton.TabIndex = 8;
+            this.CalculateButton.Text = "Calculate";
+            this.CalculateButton.UseVisualStyleBackColor = true;
+            this.CalculateButton.Click += new System.EventHandler(this.CalculateButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(142, 154);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(75, 23);
+            this.ExitButton.TabIndex = 9;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // invoiceTotalForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(265, 202);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.CalculateButton);
+            this.Controls.Add(this.totalTextBox);
+            this.Controls.Add(this.discountAmountTextBox);
+            this.Controls.Add(this.discountPercentTextBox);
+            this.Controls.Add(this.subTotalTextBox);
+            this.Controls.Add(this.discountAmountLabel);
+            this.Controls.Add(this.discountPercentLabel);
+            this.Controls.Add(this.totalLabel);
+            this.Controls.Add(this.subTotalLabel);
+            this.Name = "invoiceTotalForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.invoiceTotalForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label subTotalLabel;
+        private System.Windows.Forms.Label totalLabel;
+        private System.Windows.Forms.Label discountPercentLabel;
+        private System.Windows.Forms.Label discountAmountLabel;
+        private System.Windows.Forms.TextBox subTotalTextBox;
+        private System.Windows.Forms.TextBox discountPercentTextBox;
+        private System.Windows.Forms.TextBox discountAmountTextBox;
+        private System.Windows.Forms.TextBox totalTextBox;
+        private System.Windows.Forms.Button CalculateButton;
+        private System.Windows.Forms.Button ExitButton;
+    }
+}
+
---
+++ WindowsFormsApplication1/invoiceTotalForm.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class invoiceTotalForm : Form
+    {
+        public invoiceTotalForm()
+        {
+            InitializeComponent();
+        }
+
+        private void invoiceTotalForm_Load(object sender, EventArgs e)
+        {
+            ResetSubTotalMethod();
+        }
+
+        private void ResetSubTotalMethod()
+        {
+            subTotalTextBox.Focus();
+            subTotalTextBox.Text = "0";
+            subTotalTextBox.SelectAll();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// CalculateButton Event Handler
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void CalculateButton_Click(object sender, EventArgs e)
+        {
+            CalculateTotal();
+
+        }
+
+
+        /// <summary>
+        /// Calculating total after discount 0.1%
+        /// </summary>
+        private void CalculateTotal()
+        {
+            const double DiscountPercent = 0.1;
+            double SubTotal;
+            double DiscountAmount;
+            double Total;
+            try
+            {
+                //Variables
+                SubTotal = Convert.ToDouble(subTotalTextBox);
+                DiscountAmount = SubTotal * DiscountPercent;
+                Total = SubTotal - DiscountAmount;
+
+
+                discountAmountTextBox.Text = DiscountAmount.ToString("C2");
+                totalTextBox.Text = Total.ToString("C2");
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show("Invalid Data Entered", "Input Error");
+                Debug.WriteLine(exception.Message);
+                subTotalTextBox.Focus();
+                subTotalTextBox.SelectAll();
+                ResetSubTotalMethod();
+            }
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
